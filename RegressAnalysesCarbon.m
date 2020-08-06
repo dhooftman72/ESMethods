afd01a8c81244436a8cc0dd0d154bc3d9bb4b02e
Danny Hooftman <58477073+dhooftman72@users.noreply.github.com>
2020-08-06 09:20:55 +0200

    Add files via upload

--- RegressAnalysesCarbon.m
+++ RegressAnalysesCarbon.m
@@ -1,25 +1,29 @@
 function RegressAnalysesCarbon
+% % Do once then comment since loaded below
+% Ensembles = ReadEnsembles;
+% VariableS = ReadVariables;
+% [Ensembles, VariableS] = CleanDataSetCarbon(Ensembles,VariableS); %#ok<ASGLU>
+% save('TheDataperKM','Ensembles','VariableS')
+%% Regression analyses
 if matlabpool('size') ~= 0
     evalc('matlabpool close');
 end
-% Do once then comment since loaded below
-% Ensembles = ReadEnsembles;
-% Variables = ReadVariables;
-% [Ensembles, Variables] = CleanDataSetCarbon(Ensembles,Variables);
-% save('TheDataperKM','Ensembles','Variables')
-%% Regression analyses
-clear all
+%load('ResultsCarbonNew');
 EnsemblesNames = [{'CrossAIC'},{'CrossCross'},{'CrossDev'},...
     {'CrossLeast'},{'CrossRho'},{'UniqueDown'},{'UniqueUp'},...
     {'GridSize'},{'Mean'},{'Median'},{'PCA'},{'Regress2Median'},...
-    {'SEMamongEnsembles'},{'STD_among_Models'},{'CVamongEns'},{'CVamongModels'}];
-VariableNames = [{'RainfallAnnual'},{'PETAnnual'},{'PopSizeLog'},{'AgriculuturalPerc'},{'GrasslandPerc'},...
-    {'PeatPerc'},{'UrbanPerc'},{'WoodlandPerc'},{'TemperatureMeanAnnual'},{'TemperatureMin'},...
-    {'TemperatureSeasonality'},{'TemperatureRange'},{'RainfallSeasonality'},{'DEMMean'},{'SlopesMean'}];
+    {'SEMamongEnsembles'},{'SEM_among_Models'},{'CVamongEns'},{'CVamongModels'}];
+VariableNames = [{'PopSizeLog'},{'RainfallAnnual'},{'PETAnnual'},{'RainfallSeasonality'},...
+                    {'TemperatureMeanAnnual'},{'TemperatureMin'},{'TemperatureSeasonality'},...
+                    {'TemperatureRange'},{'DEMMean'},{'SlopesMean'},... 
+                    {'AgriculuturalPerc'},{'GrasslandPerc'},...
+                    {'PeatPerc'},{'UrbanPerc'},{'WoodlandPerc'}];
 VariList = 1:length(VariableNames);
-run_max = 10000;
-matlabpool open 'Full'
-for ensem = 15%length(EnsemblesNames)
+run_max = 100;
+if matlabpool('size') == 0
+    matlabpool open Full
+end
+for ensem = 13:14%5%length(EnsemblesNames)
     load('TheDataperKM.mat')
     clear TargetEnsemble
     TargetEnsemble = eval( ['Ensembles.',char(EnsemblesNames(ensem))]);
@@ -27,86 +31,101 @@ for ensem = 15%length(EnsemblesNames)
         str = sprintf('      Running variable %s in Ensemble %s',char(VariableNames(vari)),char(EnsemblesNames(ensem)));
         clc
         disp(str)
-        obssnames = [{'Longitude'},{'Lattitude'},{'Nr_models'},{'LongxLat'},VariableNames(vari),{'Error'}];
+        obssnames = [{'SpatialAutoCorrect'},VariableNames(vari),{'Error'}];
         clear TargetVariable
-        TargetVariable =  eval( ['Variables.',char(VariableNames(vari))]);
+        TargetVariable =  eval( ['VariableS.',char(VariableNames(vari))]);
         if min(TargetVariable)<0
             TargetVariable = TargetVariable+min(TargetVariable);
         end
         TarVar = normaVar(TargetVariable);
-        LongitudeNorm = normaVar(Variables.Longitude);
-        LattitudeNorm = normaVar(Variables.Lattitude);
-        parfor i = 1:run_max
-            draw = randperm(length(Variables.Longitude),519);
+        LongitudeNorm = VariableS.Longitude;
+        LattitudeNorm = VariableS.Lattitude;
+        len = length(VariableS.Longitude);
+        parfor i = 1:run_max 
+            draw = randperm(len,519);
             Y = TargetEnsemble(draw); %#ok<PFBNS>
-            Longitude = LongitudeNorm(draw); %#ok<PFBNS>
+            Longitude =  LongitudeNorm(draw); %#ok<PFBNS>
             Lattitude = LattitudeNorm(draw); %#ok<PFBNS>
-            NrofModels = Variables.NrModels(draw);
-            % correcting factors individual R2
-            dsInd = dataset(Y,Longitude);
-            mdltmp = LinearModel.fit(dsInd,'Y ~ Longitude');
-            R_long = min(max(mdltmp.Rsquared.Adjusted,0),1);
-            %clear mdltmp dsInd 
-            dsInd = dataset(Y,Lattitude);
-            mdltmp = LinearModel.fit(dsInd,'Y ~ Lattitude');
-            R_lat = min(max(mdltmp.Rsquared.Adjusted,0),1);
-            %clear mdltmp dsInd 
-            dsInd = dataset(Y,NrofModels);
-            mdltmp = LinearModel.fit(dsInd,'Y ~ NrofModels');
-            R_Models = min(max(mdltmp.Rsquared.Adjusted,0),1);
-            %clear mdltmp dsInd   
+            XTarget =  TarVar(draw); %#ok<PFBNS>
+    
+            [~,~,~,wij] = Morans(Longitude,Lattitude,250, Y,1); 
+            %AutoCorrelation = Autoset(AutoCorrelation,Size,{'OverallAccuracy'},Morans_I,PValue,ZValue,1);
+            Auto = zeros(519,1);
+            for s = 1:519
+                Autot = 0;
+                for t = 1:519
+                    if s ~= t
+                        Autot = Autot + (wij(s,t).*Y(t));
+                    end
+                end
+                Auto(s,1) = Autot./sum(wij(s,:)); %#ok<*AGROW>
+            end
+            % Determinant test
             
-            % correcting models
-            ds = dataset(Y,Longitude,Lattitude,NrofModels);
-            mdl = LinearModel.fit(ds,'Y ~ NrofModels + Lattitude+ Longitude + Longitude:Lattitude');
+             ds = dataset(Y,Auto);
+            mdl = LinearModel.fit(ds,'Y ~ Auto');
             table = anova(mdl,'component');
             
-            % Determinant test
             Yresid = mdl.Residuals.Raw;
-            XTarget = TargetVariable(draw); %#ok<PFBNS>
             ds2 = dataset(Yresid,XTarget);
             mdl2 = LinearModel.fit(ds2,'Yresid ~ XTarget');
-            tbl2 = anova(mdl2,'component');
-            
-            SS(:,i)= [double(table(1,1));double(table(2,1));double(table(3,1));double(table(4,1));double(tbl2(1,1));double(tbl2(2,1))]; %#ok<*SAGROW>
-            DF(:,i)=[double(table(1,2));double(table(2,2));double(table(3,2));double(table(4,2));double(tbl2(1,2));double(tbl2(2,2))];
-            F(:,i)=[double(table(1,4));double(table(2,4));double(table(3,4));double(table(4,4));double(tbl2(1,4));double(tbl2(2,4))];
-            P(:,i)=[double(table(1,5));double(table(2,5));double(table(3,5));double(table(4,5));double(tbl2(1,5));double(tbl2(2,5))];
-            R(:,i) = [R_long;R_lat;R_Models;0;(min(max(mdl2.Rsquared.Adjusted,0),1));0];
-            Direction(:,i)= [mdl.Coefficients.Estimate(2,1);mdl.Coefficients.Estimate(3,1);mdl.Coefficients.Estimate(4,1);...
-                mdl.Coefficients.Estimate(5,1);mdl2.Coefficients.Estimate(2,1);0];
+            tbl2 = anova(mdl2,'component',1);
+
+            SS(:,i)= [double(table(1,1));double(tbl2(1,1));double(tbl2(2,1))]; %#ok<*SAGROW>
+            DF(:,i)= [double(table(1,2));double(tbl2(1,2));double(tbl2(2,2))];
+            %F(:,i)=[double(table(1,4));double(tbl2(1,4));double(tbl2(2,4))];
+            %P(:,i)=[double(table(1,5));double(tbl2(1,5));double(tbl2(2,5))];
+            R(:,i) = [(min(max(mdl.Rsquared.Adjusted,0),1));(min(max(mdl2.Rsquared.Adjusted,0),1));0];
+            Direction(:,i)= [mdl.Coefficients.Estimate(2,1);mdl2.Coefficients.Estimate(2,1);0];
+            Intercept(:,i) = [mdl.Coefficients.Estimate(1,1);mdl2.Coefficients.Estimate(1,1);0];
         end
         if vari == VariList(1)
-            Stats = dataset(nanmean(SS,2),'ObsNames',obssnames','VarNames',{'SumofSquares'});
-            Stats.DF = nanmean(DF,2);
-            Stats.MeanSquares = nanmean((SS./DF),2);
-            Stats.Fvalue = nanmean(F,2);
-            Stats.Pvalue = nanmean(P,2);
-            Stats.R = nanmean(R,2);
-            Stats.Direction = nanmean(Direction,2);
+            Stats = dataset(nanmedian(SS,2),'ObsNames',obssnames','VarNames',{'SumofSquares'});
+            Stats.DF = nanmedian(DF,2);
+            Stats.MeanSquares = nanmedian((SS./DF),2);
+            Stats.Fvalue = Stats.MeanSquares./Stats.MeanSquares(3);
+            Stats.Pvalue = fpdf(Stats.Fvalue,Stats.DF,Stats.DF(3));
+            Stats.R = nanmedian(R,2);
+            Stats.Direction = nanmedian(Direction,2);
+            Stats.Intercept = nanmedian(Intercept,2);
         else
-            New = dataset(nanmean(SS(5,:),2),'ObsNames',char(VariableNames(vari)),'Varnames',{'SumofSquares'});
-            New.DF = nanmean(DF(5,:),2);
-            New.MeanSquares = nanmean((SS(5,:)./DF(5,:)),2);
-            New.Fvalue = nanmean(F(5,:),2);
-            New.Pvalue = nanmean(P(5,:),2);
-            New.R =  nanmean(R(5,:),2);
-            New.Direction = nanmean(Direction(5,:),2);
+            New = dataset(nanmedian(SS(2,:),2),'ObsNames',char(VariableNames(vari)),'Varnames',{'SumofSquares'});
+            New.DF = nanmedian(DF(2,:),2);
+            New.MeanSquares = nanmedian((SS(2,:)./DF(2,:)),2);
+            New.Fvalue = New.MeanSquares./(nanmedian((SS(3,:)./DF(3,:)),2));
+            New.Pvalue = fpdf(New.Fvalue,New.DF,nanmedian(DF(3,:),2));
+            New.R =  nanmedian(R(2,:),2);
+            New.Direction = nanmedian(Direction(2,:),2);
+            New.Intercept = nanmedian(Intercept(2,:),2);
             Stats(char(VariableNames(vari)),:) = New;
         end
-        clear SS DF F P R  TargetVariable New
+        clear SS DF F P R  TargetVariable New 
     end
     Results.(genvarname(char(EnsemblesNames(ensem)))) = Stats;
     clearvars -except Results EnsemblesNames VariableNames VariList run_max
 end
 save('ResultsCarbonNew','Results','EnsemblesNames', 'VariableNames', 'VariList');
-matlabpool close
+if matlabpool('size') ~= 0
+    matlabpool close
+end
+end
+
+
+function TarVar = normaVar(TarVar)
+x_range_perc_low = prctile(TarVar(:,1),2.5);
+x_range_norm = TarVar(:,1) - x_range_perc_low;
+x_range_norm(x_range_norm<0) = 0;
+x_range_perc = prctile(x_range_norm,97.5);
+x_range = (x_range_norm./x_range_perc);
+x_range(x_range>1) = 1;
+TarVar = x_range;
+clear x_range*
 end
 
 function Ensembles = ReadEnsembles
 % Read all Variables
 % Read list
-cd Ensemble layers
+cd Ensemble_layers
 list = dir;
 length_names = length(list);
 count = 1;
@@ -149,6 +168,7 @@ end
 
 function  Variables = ReadVariables
 % Read list
+clear all
 list = dir;
 length_names = length(list);
 count = 1;
@@ -227,6 +247,14 @@ end
 fclose('all');
 end
 
+function AutoCorrelation = Autoset(AutoCorrelation,size,txt,Morans_I,PValue,ZValue,i)
+    AutoCorrelation.Set(i,1) = txt; 
+    AutoCorrelation.N(i,1) = size;
+AutoCorrelation.MoransI(i,1) = Morans_I;
+AutoCorrelation.ZValue(i,1) = ZValue;
+AutoCorrelation.PValue(i,1) = PValue;
+end
+
 function [Ensembles, Variables] = CleanDataSetCarbon(Ensembles,Variables)
 Ensembles_dbl = double(Ensembles);
 Variables_dbl = double(Variables);
@@ -246,27 +274,28 @@ Ensembles_new.Median = Ensembles_dbl(:,9);
 Ensembles_new.PCA = Ensembles_dbl(:,10);
 Ensembles_new.Regress2Median = Ensembles_dbl(:,11);
 Ensembles_new.SEMamongEnsembles = Ensembles_dbl(:,12);
-Ensembles_new.Lattitude = Variables_dbl(:,5);
-Ensembles_new.Longitude = Variables_dbl(:,6);
-Variables_new = dataset(Variables_dbl(:,6),'Varnames','Longitude');
-Variables_new.Lattitude = Variables_dbl(:,5);
-Variables_new.CarbonZones = Variables_dbl(:,3);
-Variables_new.RainfallAnnual = Variables_dbl(:,15);
-Variables_new.PETAnnual = Variables_dbl(:,12);
-Variables_new.PopSize = Variables_dbl(:,13);
+Ensembles_new.Lattitude = Variables_dbl(:,4);
+Ensembles_new.Longitude = Variables_dbl(:,5);
+
+Variables_new = dataset(Variables_dbl(:,5),'Varnames','Longitude');
+Variables_new.Lattitude = Variables_dbl(:,4);
+Variables_new.CarbonZones = Variables_dbl(:,2);
+Variables_new.RainfallAnnual = Variables_dbl(:,14);
+Variables_new.PETAnnual = Variables_dbl(:,11);
+Variables_new.PopSize = Variables_dbl(:,12);
 Variables_new.AgriculuturalPerc = Variables_dbl(:,1)./1600;
-Variables_new.GrasslandPerc = Variables_dbl(:,4)./1600;
-Variables_new.PeatPerc = Variables_dbl(:,11)./1600;
-Variables_new.UrbanPerc = Variables_dbl(:,19)./1600;
-Variables_new.WoodlandPerc = Variables_dbl(:,20)./1600;
-Variables_new.TemperatureMeanAnnual = Variables_dbl(:,7);
-Variables_new.TemperatureMin = Variables_dbl(:,9);
-Variables_new.TemperatureSeasonality = Variables_dbl(:,18);
-Variables_new.TemperatureRange = Variables_dbl(:,16);
-Variables_new.RainfallSeasonality = Variables_dbl(:,14);
-Variables_new.NrModels = Variables_dbl(:,10);
-Variables_new.DEMMean = Variables_dbl(:,8);
-Variables_new.SlopesMean = Variables_dbl(:,17);
+Variables_new.GrasslandPerc = Variables_dbl(:,3)./1600;
+Variables_new.PeatPerc = Variables_dbl(:,10)./1600;
+Variables_new.UrbanPerc = Variables_dbl(:,18)./1600;
+Variables_new.WoodlandPerc = Variables_dbl(:,19)./1600;
+Variables_new.TemperatureMeanAnnual = Variables_dbl(:,6);
+Variables_new.TemperatureMin = Variables_dbl(:,8);
+Variables_new.TemperatureSeasonality = Variables_dbl(:,17);
+Variables_new.TemperatureRange = Variables_dbl(:,15);
+Variables_new.RainfallSeasonality = Variables_dbl(:,13);
+Variables_new.NrModels = Variables_dbl(:,9);
+Variables_new.DEMMean = Variables_dbl(:,7);
+Variables_new.SlopesMean = Variables_dbl(:,16);
 Ensembles = Ensembles_new;
 Variables = Variables_new;
 end
